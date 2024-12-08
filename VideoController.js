87170321e66c40ab8876e175eba272f0f06af3cd
ksenera <ksenera7130@conestogac.on.ca>
2024-12-08 18:44:12 -0500

    created a new project for final Project. Created a new class VideoController.js and it's constructor will be using MVC pattern for the video stream

---
+++ VideoController.js
@@ -0,0 +1,17 @@
+/** 
+ * CLASS       : VideoController.js 
+ * 
+ * DESCRIPTION : Controller class for the video stream, handles user interactions and updates the model. 
+ * 
+ */
+
+
+export default class VideoStream {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.video = createCapture(VIDEO);
+      this.video.size(width, height);
+      this.video.hide();
+    }
+}
