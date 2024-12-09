19a3f8e3c328da97e246b30d3ff9d88b9eeccb5e
ksenera <ksenera7130@conestogac.on.ca>
2024-12-08 22:24:08 -0500

    added switch case for choosing type of filter logic. gui class will be created next to deal with the drop down menu for the GUI

--- FilterModel.js
+++ FilterModel.js
@@ -20,4 +20,44 @@ class FilterModel {
             blur: (frame) => this.applyBlur(frame),
         };
     }
+
+    /**
+     * Function         : applyFilterType() 
+     * Description      : uses switch case to apply a filter on the filter canvas.
+     * Parameters       : filterType - 
+     *                    frame - 
+     * Return           ;
+     */
+    applyFilterType(filterType, frame) {
+        this.filterCanvas.image(frame, 0, 0, this.videoWidth, this.videoHeight);
+
+        switch (filterType) {
+        case "invert":
+            this.filterCanvas.loadPixels();
+            for (let i = 0; i < this.filterCanvas.pixels.length; i += 4) {
+            this.filterCanvas.pixels[i] = 255 - this.filterCanvas.pixels[i]; 
+            this.filterCanvas.pixels[i + 1] = 255 - this.filterCanvas.pixels[i + 1]; 
+            this.filterCanvas.pixels[i + 2] = 255 - this.filterCanvas.pixels[i + 2]; 
+            }
+            this.filterCanvas.updatePixels();
+            break;
+
+        case "gray":
+            this.filterCanvas.filter(GRAY);
+            break;
+
+        case "posterize":
+            this.filterCanvas.filter(POSTERIZE, 3);
+            break;
+
+        case "blur":
+            this.filterCanvas.filter(BLUR, 3);
+            break;
+
+        default:
+            break;
+        }
+
+        return this.filterCanvas;
+    }
 }
