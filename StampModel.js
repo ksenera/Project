b15c87cf3b8e88da42c75a1fcd6c6fc138cb52a1
ksenera <ksenera7130@conestogac.on.ca>
2024-12-09 15:10:55 -0500

    added the addstamp and helper function placeinvideobounds to add stamps to the array

--- StampModel.js
+++ StampModel.js
@@ -11,6 +11,7 @@ class StampModel {
         this.videoHeight = videoHeight;
         this.selectedStamp = null; // 
         this.stamps = []; // array for the stamped stamps on video feed
+        this.stampsCanvas = createGraphics(videoWidth, videoHeight); // new canvas for stamps 
     }
 
     // method for selecting and deselecting stamps 
@@ -25,6 +26,30 @@ class StampModel {
     // method for adding stamp to video 
     addStamp(position) {
         // check video bounds if loop 
+        if (!this.selectedStamp){
+            return;
+        }
+        // position is within bounds of video stream 
+        const { x, y} = this.placeInVideoBounds(position);
+
+        // push to stamps[] 
+        this.stamps.push({
+            image: this.selectedStamp,
+            position: { x, y}, 
+        });
+
+        this. stampsCanvas.image(this.selectedStamp, x, y, 150, 150);
     }
 
+    // helper function to keep position within video stream boundary 
+    placeInVideoBounds(position) {
+        const x = Math.min(Math.max(position.x - 75, 0), this.videoWidth - 150);
+        const y = Math.min(Math.max(position.y - 75, 0), this.videoHeight - 150);
+        return { x, y };
+    }
+
+    // render stamps 
+    renderStamps() {
+        return this.stamps;
+    }
 }
