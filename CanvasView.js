52b9ff60c405b9ac4213c2797845582c108f93dc
ksenera <ksenera7130@conestogac.on.ca>
2024-12-08 20:26:29 -0500

    updated canvasview class to render video frames

--- CanvasView.js
+++ CanvasView.js
@@ -13,3 +13,22 @@ export default class CanvasView {
       this.videoWidth = videoWidth;
       this.videoHeight = videoHeight;
     }
+    
+    updateDimensions(windowWidth, windowHeight) {
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
+    }
+
+    render(videoFrame) {
+        // find center position of the video stream
+        const x = (this.windowWidth - this.videoWidth) / 2;
+        const y = (this.windowHeight - this.videoHeight) / 2;
+
+        // render video frame 
+        image(videoFrame, x, y, this.videoWidth, this.videoHeight);
+
+        noFill();
+        stroke(0);
+        rect(x, y, this.videoWidth, this.videoHeight);
+    }
+}
