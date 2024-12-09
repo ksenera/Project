d062e88a173d288b3ba2881b0bd6259fa72f4769
ksenera <ksenera7130@conestogac.on.ca>
2024-12-08 20:28:58 -0500

    added the import statement and instantiated canvas to render in draw()

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
 import VideoStream from './controllers/VideoController.js';
+import CanvasView from './views/CanvasView.js';
 
 let videoStream;
+let canvasView;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
     // Initializing the video stream and canvas view
     videoStream = new VideoStream(640, 480);
+    canvasView = new CanvasView(windowWidth, windowHeight, 640, 480);
+
+    videoStream.start();
     
 }
 
 function draw() {
   background(220);
+
+  const videoFrame = videoStream.getFrame();
+
+  canvasView.render(videoFrame);
 }
