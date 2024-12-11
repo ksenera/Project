5ae437bd9835dbfb029fa67d74f7af8ed714aebf
ksenera <ksenera7130@conestogac.on.ca>
2024-12-11 02:06:06 -0500

    FIXED THE NEGATIVE RADIUS ERROR FOR THE ELLIPSE

--- RenderShape.js
+++ RenderShape.js
@@ -20,12 +20,24 @@ class RenderShape {
         canvas.stroke(shape.strokeColor);
         canvas.strokeWeight(shape.strokeWeight);
 
+        let x1 = shape.startX;
+        let y1 = shape.startY;
+        let x2 = shape.endX;
+        let y2 = shape.endY;
+
+        if (x2 < x1) {
+            [x1, x2] = [x2, x1];
+        }
+        if (y2 < y1) {
+            [y1, y2] = [y2, y1];
+        }
+
         if (shape.type === 'rectangle') {
             canvas.rectMode(CORNERS);
-            canvas.rect(shape.startX, shape.startY, shape.endX, shape.endY);
+            canvas.rect(x1, y1, x2, y2);
         } else if (shape.type === 'ellipse') {
             canvas.ellipseMode(CORNERS);
-            canvas.ellipse(shape.startX, shape.startY, shape.endX, shape.endY);
+            canvas.ellipse(x1, y1, x2, y2);
         }
         canvas.pop();
     }
