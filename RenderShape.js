2a504333da1fd592c5b970e4f000193ee54f5aa5
ksenera <ksenera7130@conestogac.on.ca>
2024-12-11 22:14:56 -0500

    cleaning the code for rendering shapes

--- RenderShape.js
+++ RenderShape.js
@@ -12,7 +12,7 @@ class RenderShape {
      * Description: Renders a rectangle or ellipse 
      * 
      */
-    static renderShape(targetCanvas, shape, isVisualFeedbackVisible = false) {
+    static renderShape(targetCanvas, shape) {
         const canvas = targetCanvas || window; 
         canvas.push();
 
@@ -21,28 +21,18 @@ class RenderShape {
         if (shape.strokeWeight === 0) canvas.noStroke();
         else canvas.strokeWeight(shape.strokeWeight);
 
-        if (shape.type === 'rectangle') {
-            canvas.rectMode(CORNERS);
-            canvas.rect(
-                shape.startX,
-                shape.startY,
-                isVisualFeedbackVisible ? shape.endX : shape.endX,
-                isVisualFeedbackVisible ? shape.endY : shape.endY
-            );
-        } else if (shape.type === 'ellipse') {
-            if (shape.endX !== shape.startX || shape.endY !== shape.startY) {
-                const x = Math.min(shape.startX, shape.endX);
-                const y = Math.min(shape.startY, shape.endY);
+        if (shape.endX !== shape.startX || shape.endY !== shape.startY) {
+            if (shape.type === 'rectangle') {
+                canvas.rectMode(CORNERS);
+                canvas.rect(shape.startX, shape.startY, shape.endX, shape.endY);
+            } else if (shape.type === 'ellipse') {
                 const width = Math.abs(shape.endX - shape.startX);
                 const height = Math.abs(shape.endY - shape.startY);
+                const x = Math.min(shape.startX, shape.endX);
+                const y = Math.min(shape.startY, shape.endY);
                 
-            canvas.ellipseMode(CENTER);
-            canvas.ellipse(
-                    x + width/2,  
-                    y + height/2, 
-                    width,      
-                    height        
-                );
+                canvas.ellipseMode(CENTER);
+                canvas.ellipse(x + width/2, y + height/2, width, height);
             }
         }
         canvas.pop();
